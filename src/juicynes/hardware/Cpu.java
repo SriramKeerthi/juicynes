037c887bd055be34a78d4c4af2ee54a035fa9b9a
stantonwhite <stantonwhite@744ec148-a6df-11dd-a8c6-677c70d10fe4>
2008-11-15 05:20:13 +0000

    Added cycle information to all opcodes.  Added nestest which checks all cpu opcodes for correctness.

--- src/juicynes/hardware/Cpu.java
+++ src/juicynes/hardware/Cpu.java
@@ -20,462 +20,244 @@ public class Cpu
 	boolean overflowFlag;
 	boolean negativeFlag;
 	
-	
 	int[] memory;
 	
+	int cycles;
+	int opcode;
+	boolean pageCrossed;
 	
 	public void run()
 	{
-		
-		int opcode = 0;
-		
-		switch (opcode)
+		while(true)
 		{
-		//TODO: Change load operations to have address as param instead of value.
+			opcode = memory[pc++];
+			switch (opcode)
+			{
+			// (Load/Store Operations)
+			case 0xA9: lda(immediate()); cycles += 2; break;
+			case 0xA5: lda(zeropage());  cycles += 3; break;
+			case 0xB5: lda(zeropageX()); cycles += 4; break;
+			case 0xAD: lda(absolute());  cycles += 4; break;
+			case 0xBD: lda(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0xB9: lda(absoluteY()); cycles += pageCrossed?5:4; break;
+			case 0xA1: lda(indirectX()); cycles += 6; break;
+			case 0xB1: lda(indirectY()); cycles += pageCrossed?6:5; break;
+			
+			case 0xA2: ldx(immediate()); cycles += 2; break;
+			case 0xA6: ldx(zeropage());  cycles += 3; break;
+			case 0xB6: ldx(zeropageY()); cycles += 4; break;
+			case 0xAE: ldx(absolute());  cycles += 4; break;
+			case 0xBE: ldx(absoluteY()); cycles += pageCrossed?5:4; break;
+			
+			case 0xA0: ldy(immediate()); cycles += 2; break;
+			case 0xA4: ldy(zeropage());  cycles += 3; break;
+			case 0xB4: ldy(zeropageX()); cycles += 4; break;
+			case 0xAC: ldy(absolute());  cycles += 4; break;
+			case 0xBC: ldy(absoluteX()); cycles += pageCrossed?5:4; break;
+			
+			case 0x85: sta(zeropage());  cycles += 3; break;
+			case 0x95: sta(zeropageX()); cycles += 4; break;
+			case 0x8D: sta(absolute());  cycles += 4; break;
+			case 0x9D: sta(absoluteX()); cycles += 5; break;
+			case 0x99: sta(absoluteY()); cycles += 5; break;
+			case 0x81: sta(indirectX()); cycles += 6; break;
+			case 0x91: sta(indirectY()); cycles += 6; break;
+			
+			case 0x86: stx(zeropage());  cycles += 3; break;
+			case 0x96: stx(zeropageY()); cycles += 4; break;
+			case 0x8E: stx(absolute());  cycles += 4; break;
+			
+			case 0x84: sty(zeropage());  cycles += 3; break;
+			case 0x94: sty(zeropageX()); cycles += 4; break;
+			case 0x8C: sty(absolute());  cycles += 4; break;
+			
+			case 0xAA: tax(); cycles += 2; break; 
+			case 0xA8: tay(); cycles += 2; break;
+			case 0x8A: txa(); cycles += 2; break;
+			case 0x98: tya(); cycles += 2; break;
+			
+			// (Stack Operations)
+			case 0xBA: tsx(); cycles += 2; break;
+			case 0x9A: txs(); cycles += 2; break;
+			case 0x48: pha(); cycles += 3; break; 
+			case 0x08: php(); cycles += 3; break;
+			case 0x68: pla(); cycles += 4; break;
+			case 0x28: plp(); cycles += 4; break;
+			
+			// (Logical Operations)
+			case 0x29: and(immediate()); cycles += 2; break;
+			case 0x25: and(zeropage());  cycles += 3; break;
+			case 0x35: and(zeropageX()); cycles += 4; break;
+			case 0x2D: and(absolute());  cycles += 4; break;
+			case 0x3D: and(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0x39: and(absoluteY()); cycles += pageCrossed?5:4; break;
+			case 0x21: and(indirectX()); cycles += 6; break;
+			case 0x31: and(indirectY()); cycles += pageCrossed?6:5; break;
 		
-		// (Load/Store Operations)
-		// --- LDA --- //
-		case 0xA9:
-			lda( memory[immediate()] );
-		break;
-		case 0xA5:
-			lda( memory[zeropage()] );
-		break;
-		case 0xB5:
-			lda( memory[zeropageX()] );
-		break;
-		case 0xAD:
-			lda( memory[absolute()] );
-		break;
-		case 0xBD:
-			lda( memory[absoluteX()] );
-		break;
-		case 0xB9:
-			lda( memory[absoluteY()] );
-		break;
-		case 0xA1:
-			lda( memory[indirectX()] );
-		break;
-		case 0xB1:
-			lda( memory[indirectY()] );
-		break;
-		
-		// --- LDX --- //
-		case 0xA2:
-			ldx( memory[immediate()] );
-		break;
-		case 0xA6:
-			ldx( memory[zeropage()] );
-		break;
-		case 0xB6:
-			ldx( memory[zeropageY()] );
-		break;
-		case 0xAE:
-			ldx( memory[absolute()] );
-		break;
-		case 0xBE:
-			ldx( memory[absoluteY()] );
-		break;
-		
-		// --- LDY --- //
-		case 0xA0:
-			ldx( memory[immediate()] );
-		break;
-		case 0xA4:
-			ldx( memory[zeropage()] );
-		break;
-		case 0xB4:
-			ldx( memory[zeropageX()] );
-		break;
-		case 0xAC:
-			ldx( memory[absolute()] );
-		break;
-		case 0xBC:
-			ldx( memory[absoluteX()] );
-		break;
-		
-		// --- STA --- //
-		case 0x85:
-			sta( zeropage() );
-		break;
-		case 0x95:
-			sta( zeropageX() );
-		break;
-		case 0x8D:
-			sta( absolute() );
-		break;
-		case 0x9D:
-			sta( absoluteX() );
-		break;
-		case 0x99:
-			sta( absoluteY() );
-		break;
-		case 0x81:
-			sta( indirectX() );
-		break;
-		case 0x91:
-			sta( indirectY() );
-		break;
-		
-		// --- STX --- //
-		case 0x86:
-			stx( zeropage() );
-		break;
-		case 0x96:
-			stx( zeropageY() );
-		break;
-		case 0x8E:
-			stx( absolute() );
-		break;
-		
-		// --- STY --- //
-		case 0x84:
-			sty( zeropage() );
-		break;
-		case 0x94:
-			sty( zeropageX() );
-		break;
-		case 0x8C:
-			sty( absolute() );
-		break;
-		
-		case 0xAA:
-			tax();
-		break;
-		case 0xA8:
-			tay();
-		break;
-		case 0x8A:
-			txa();
-		break;
-		
-		case 0x98:
-			tya();
-		break;
-		
-		// (Stack Operations)
-		case 0xBA:
-			tsx();
-		break;
-		case 0x9A:
-			txs();
-		break;
-		case 0x48:
-			pha();
-		break;
-		case 0x08:
-			php();
-		break;
-		case 0x68:
-			pla();
-		break;
-		case 0x28:
-			plp();
-		break;
-		
-		// (Logical Operations)
-		// and
-		case 0x29:
-			and( immediate() );
-		break;
-		case 0x25:
-			and( zeropage() );
-		break;
-		case 0x35:
-			and( zeropageX() );
-		break;
-		case 0x2D:
-			and( absolute() );
-		break;
-		case 0x3D:
-			and( absoluteX() );
-		break;
-		case 0x39:
-			and( absoluteY() );
-		break;
-		case 0x21:
-			and( indirectX() );
-		break;
-		case 0x31:
-			and( indirectY() );
-		break;
-	
-		// eor
-		case 0x49:
-			eor( immediate() );
-		break;
-		case 0x45:
-			eor( zeropage() );
-		break;
-		case 0x55:
-			eor( zeropageX() );
-		break;
-		case 0x4D:
-			eor( absolute() );
-		break;
-		case 0x5D:
-			eor( absoluteX() );
-		break;
-		case 0x41:
-			eor( indirectX() );
-		break;
-		case 0x51:
-			eor( indirectY() );
-		break;
-		
-		// ora
-		case 0x09:
-			ora( immediate() );
-		break;
-		case 0x05:
-			ora( zeropage() );
-		break;
-		case 0x15:
-			ora( zeropageX() );
-		break;
-		case 0x0D:
-			ora( absolute() );
-		break;
-		case 0x1D:
-			ora( absoluteX() );
-		break;
-		case 0x19:
-			ora( absoluteY() );
-		break;
-		case 0x01:
-			ora( indirectX() );
-		break;
-		case 0x11:
-			ora( indirectY() );
-		break;
-		
-		// bit
-		case 0x24:
-			bit( zeropage() );
-		break;
-		case 0x2C:
-			bit( absolute() );
-		break;
-		
-		// (Arithmetic Operations)
-		// adc
-		case 0x69:
-			adc( immediate() );
-		break;
-		case 0x65:
-			adc( zeropage() );
-		break;
-		case 0x75:
-			adc( zeropageX() );
-		break;
-		case 0x6D:
-			adc( absolute() );
-		break;
-		case 0x7D:
-			adc( absoluteX() );
-		break;
-		case 0x79:
-			adc( absoluteY() );
-		break;
-		case 0x61:
-			adc( indirectX() );
-		break;
-		case 0x71:
-			adc( indirectY() );
-		break;
-		
-		// sbc
-		case 0xE9:
-			sbc( immediate() );
-		break;
-		case 0xE5:
-			sbc( zeropage() );
-		break;
-		case 0xF5:
-			sbc( zeropageX() );
-		break;
-		case 0xED:
-			sbc( absolute() );
-		break;
-		case 0xFD:
-			sbc( absoluteX() );
-		break;
-		case 0xF9:
-			sbc( absoluteY() );
-		break;
-		case 0xE1:
-			sbc( indirectX() );
-		break;
-		case 0xF1:
-			sbc( indirectY() );
-		break;
-		
-		// cmp
-		case 0xC9:
-			cmp( immediate() );
-		break;
-		case 0xC5:
-			cmp( zeropage() );
-		break;
-		case 0xD5:
-			cmp( zeropageX() );
-		break;
-		case 0xCD:
-			cmp( absolute() );
-		break;
-		case 0xDD:
-			cmp( absoluteX() );
-		break;
-		case 0xD9:
-			cmp( absoluteY() );
-		break;
-		case 0xC1:
-			cmp( indirectX() );
-		break;
-		case 0xD1:
-			cmp( indirectY() );
-		break;
-		
-		// cpx
-		case 0xE0:
-			cpx( immediate() );
-		break;
-		case 0xE4:
-			cpx( zeropage() );
-		break;
-		case 0xEC:
-			cpx( absolute() );
-		break;
-		
-		// cpy
-		case 0xC0:
-			cpy( immediate() );
-		break;
-		case 0xC4:
-			cpy( zeropage() );
-		break;
-		case 0xCC:
-			cpy( absolute() );
-		break;
-		
-		// (Increment/Decrement Operations)
-		// inc
-		case 0xE6: inc(zeropage());  break;
-		case 0xF6: inc(zeropageX()); break;
-		case 0xEE: inc(absolute());  break;
-		case 0xFE: inc(absoluteX()); break;
-		
-		// inx
-		case 0xE8: inx(); break;
-		
-		// iny
-		case 0xC8: iny(); break;
-		
-		// dec
-		case 0xC6: dec(zeropage());  break;
-		case 0xD6: dec(zeropageX()); break;
-		case 0xCE: dec(absolute());  break;
-		case 0xDE: dec(absoluteX()); break;
-		
-		// dex
-		case 0xCA: dex(); break;
-		
-		// dey
-		case 0x88: dey(); break;
-		
-		// (Shift Operations)
-		// asl
-		case 0x0A: asl_accumulator(); break;
-		case 0x06: asl(zeropage());	  break;
-		case 0x16: asl(zeropageX());  break;
-		case 0x0E: asl(absolute());   break;
-		case 0x1E: asl(absoluteX());  break;
-		
-		// lsr
-		case 0x4A: lsr_accumulator(); break;
-		case 0x46: lsr(zeropage());   break;
-		case 0x56: lsr(zeropageX());  break;
-		case 0x4E: lsr(absolute());   break;
-		case 0x5E: lsr(absoluteX());  break;
-		
-		// rol
-		case 0x2A: rol_accumulator(); break;
-		case 0x26: rol(zeropage());   break;
-		case 0x36: rol(zeropageX());  break;
-		case 0x2E: rol(absolute());   break;
-		case 0x3E: rol(absoluteX());  break;
-		
-		// ror
-		case 0x6A: ror_accumulator(); break;
-		case 0x66: ror(zeropage());   break;
-		case 0x76: ror(zeropageX());  break;
-		case 0x6E: ror(absolute());   break;
-		case 0x7E: ror(absoluteX());  break;
-		
-		// (Jumps and Calls Operations)
-		// jmp
-		case 0x4C: jmp(absolute()); break;
-		case 0x6C: jmp(indirect()); break;
-		
-		// jsr
-		case 0x20: jsr(absolute()); break;
-		
-		// rts
-		case 0x60: rts(); break;
-		
-		// (Branch Operations)
-		case 0x90: bcc(); break;
-		case 0xB0: bcs(); break;
-		case 0xF0: beq(); break;
-		case 0x30: bmi(); break;
-		case 0xD0: bne(); break;
-		case 0x10: bpl(); break;
-		case 0x50: bvc(); break;
-		case 0x70: bvs(); break;
-		
-		// (Status Flag Operations)
-		case 0x18: clc(); break;
-		case 0xD8: cld(); break;
-		case 0x58: cli(); break;
-		case 0xB8: clv(); break;
-		case 0x38: sec(); break;
-		case 0xF8: sed(); break;
-		case 0x78: sei(); break;
-		
-		// (System Function Operations)
-		case 0x00: brk(); break;
-		case 0xEA: nop(); break;
-		case 0x40: rti(); break;
-		
-		// Catch bad opcodes
-		default:
-			System.out.println("Illegal opcode: " + opcode);
-		break;
+			case 0x49: eor(immediate()); cycles += 2; break;
+			case 0x45: eor(zeropage());  cycles += 3; break;
+			case 0x55: eor(zeropageX()); cycles += 4; break;
+			case 0x4D: eor(absolute());  cycles += 4; break;
+			case 0x5D: eor(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0x59: eor(absoluteY()); cycles += pageCrossed?5:4; break;
+			case 0x41: eor(indirectX()); cycles += 6; break;
+			case 0x51: eor(indirectY()); cycles += pageCrossed?6:5; break;
+			
+			case 0x09: ora(immediate()); cycles += 2; break;
+			case 0x05: ora(zeropage());  cycles += 3; break;
+			case 0x15: ora(zeropageX()); cycles += 4; break;
+			case 0x0D: ora(absolute());  cycles += 4; break;
+			case 0x1D: ora(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0x19: ora(absoluteY()); cycles += pageCrossed?5:4; break;
+			case 0x01: ora(indirectX()); cycles += 6; break;
+			case 0x11: ora(indirectY()); cycles += 5; break;
+			
+			case 0x24: bit(zeropage()); cycles += 3; break;
+			case 0x2C: bit(absolute()); cycles += 4; break;
+			
+			// (Arithmetic Operations)
+			case 0x69: adc(immediate()); cycles += 2; break;
+			case 0x65: adc(zeropage());  cycles += 3; break;
+			case 0x75: adc(zeropageX()); cycles += 4; break;
+			case 0x6D: adc(absolute());  cycles += 4; break;
+			case 0x7D: adc(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0x79: adc(absoluteY()); cycles += pageCrossed?5:4; break;
+			case 0x61: adc(indirectX()); cycles += 6; break;
+			case 0x71: adc(indirectY()); cycles += pageCrossed?6:5; break;
+			
+			case 0xE9: sbc(immediate()); cycles += 2; break;
+			case 0xE5: sbc(zeropage());  cycles += 3; break;
+			case 0xF5: sbc(zeropageX()); cycles += 4; break;
+			case 0xED: sbc(absolute());  cycles += 4; break;
+			case 0xFD: sbc(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0xF9: sbc(absoluteY()); cycles += pageCrossed?5:5; break;
+			case 0xE1: sbc(indirectX()); cycles += 6; break;
+			case 0xF1: sbc(indirectY()); cycles += pageCrossed?6:5; break;
+			
+			case 0xC9: cmp(immediate()); cycles += 2; break;
+			case 0xC5: cmp(zeropage());  cycles += 3; break;
+			case 0xD5: cmp(zeropageX()); cycles += 4; break;
+			case 0xCD: cmp(absolute());  cycles += 4; break;
+			case 0xDD: cmp(absoluteX()); cycles += pageCrossed?5:4; break;
+			case 0xD9: cmp(absoluteY()); cycles += pageCrossed?5:4; break;
+			case 0xC1: cmp(indirectX()); cycles += 6; break;
+			case 0xD1: cmp(indirectY()); cycles += pageCrossed?6:5; break;
+			
+			case 0xE0: cpx(immediate()); cycles += 2; break;
+			case 0xE4: cpx(zeropage());  cycles += 3; break;
+			case 0xEC: cpx(absolute());  cycles += 4; break;
+			
+			case 0xC0: cpy(immediate()); cycles += 2; break;
+			case 0xC4: cpy(zeropage());  cycles += 3; break;
+			case 0xCC: cpy(absolute());  cycles += 4; break;
+			
+			// (Increment/Decrement Operations)
+			case 0xE6: inc(zeropage());  cycles += 5; break;
+			case 0xF6: inc(zeropageX()); cycles += 6; break;
+			case 0xEE: inc(absolute());  cycles += 6; break;
+			case 0xFE: inc(absoluteX()); cycles += 7; break;
+			
+			case 0xE8: inx(); cycles += 2; break;			
+			case 0xC8: iny(); cycles += 2; break;
+			
+			case 0xC6: dec(zeropage());  cycles += 5; break;
+			case 0xD6: dec(zeropageX()); cycles += 6; break;
+			case 0xCE: dec(absolute());  cycles += 6; break;
+			case 0xDE: dec(absoluteX()); cycles += 7; break;
+			
+			case 0xCA: dex(); cycles += 2; break;			
+			case 0x88: dey(); cycles += 2; break;
+			
+			// (Shift Operations)
+			case 0x0A: asl_accumulator(); cycles += 2; break;
+			case 0x06: asl(zeropage());	  cycles += 5; break;
+			case 0x16: asl(zeropageX());  cycles += 6; break;
+			case 0x0E: asl(absolute());   cycles += 6; break;
+			case 0x1E: asl(absoluteX());  cycles += 7; break;
+			
+			case 0x4A: lsr_accumulator(); cycles += 2; break;
+			case 0x46: lsr(zeropage());   cycles += 5; break;
+			case 0x56: lsr(zeropageX());  cycles += 6; break;
+			case 0x4E: lsr(absolute());   cycles += 6; break;
+			case 0x5E: lsr(absoluteX());  cycles += 7; break;
+			
+			case 0x2A: rol_accumulator(); cycles += 2; break;
+			case 0x26: rol(zeropage());   cycles += 5; break;
+			case 0x36: rol(zeropageX());  cycles += 6; break;
+			case 0x2E: rol(absolute());   cycles += 6; break;
+			case 0x3E: rol(absoluteX());  cycles += 7; break;
+			
+			case 0x6A: ror_accumulator(); cycles += 2; break;
+			case 0x66: ror(zeropage());   cycles += 5; break;
+			case 0x76: ror(zeropageX());  cycles += 6; break;
+			case 0x6E: ror(absolute());   cycles += 6; break;
+			case 0x7E: ror(absoluteX());  cycles += 7; break;
+			
+			// (Jumps and Calls Operations)
+			case 0x4C: jmp(absolute()); cycles += 3; break;
+			case 0x6C: jmp(indirect()); cycles += 5; break;
+			
+			case 0x20: jsr(absolute()); cycles += 6; break;
+			
+			case 0x60: rts(); cycles += 6; break;
+			
+			// (Branch Operations)
+			case 0x90: bcc(); cycles += 2; break;
+			case 0xB0: bcs(); cycles += 2; break;
+			case 0xF0: beq(); cycles += 2; break;
+			case 0x30: bmi(); cycles += 2; break;
+			case 0xD0: bne(); cycles += 2; break;
+			case 0x10: bpl(); cycles += 2; break;
+			case 0x50: bvc(); cycles += 2; break;
+			case 0x70: bvs(); cycles += 2; break;
+			
+			// (Status Flag Operations)
+			case 0x18: clc(); cycles += 2; break;
+			case 0xD8: cld(); cycles += 2; break;
+			case 0x58: cli(); cycles += 2; break;
+			case 0xB8: clv(); cycles += 2; break;
+			case 0x38: sec(); cycles += 2; break;
+			case 0xF8: sed(); cycles += 2; break;
+			case 0x78: sei(); cycles += 2; break;
+			
+			// (System Function Operations)
+			case 0x00: brk(); cycles += 7; break;
+			case 0xEA: nop(); cycles += 2; break;
+			case 0x40: rti(); cycles += 6; break;
+			
+			// Catch bad opcodes
+			default:
+				System.out.println("Illegal opcode: " + opcode);
+			break;
+			}
 		}
 	}
 	
 	
-	/// --- Load/Store Operations --- ///
+	// (Load/Store Operations)
 	
-	void lda(int value)
+	void lda(int address)
 	{
-		adjustZeroFlag(value);
-		adjustSignFlag(value);
-		accumulator = value;
+		int val = memory[address];
+		adjustZeroFlag(val);
+		adjustSignFlag(val);
+		accumulator = val;
 	}
 	
-	void ldx(int value)
+	void ldx(int address)
 	{
-		adjustZeroFlag(value);
-		adjustSignFlag(value);
-		xindex = value;
+		int val = memory[address];
+		adjustZeroFlag(val);
+		adjustSignFlag(val);
+		xindex = val;
 	}
 	
-	void ldy(int value)
+	void ldy(int address)
 	{
-		adjustZeroFlag(value);
-		adjustSignFlag(value);
-		yindex = value;
+		int val = memory[address];
+		adjustZeroFlag(val);
+		adjustSignFlag(val);
+		yindex = val;
 	}
 	
 	void sta(int address)
@@ -910,21 +692,32 @@ public class Cpu
 	// (System Functions)
 	void brk()
 	{
-		//TODO: Finish me.
+		push(pc);
+		push(statusFlag());
+		pc = irqVector();
+		breakFlag = true;
 	}
 	
 	void nop()
 	{
-		
 	}
 	
 	void rti()
 	{
+		int statusFlags = pull();
+		pc = pull();
 		
+		carryFlag = (statusFlags & 0x01) == 0x01;
+		zeroFlag = (statusFlags & 0x02) == 0x02;
+		interruptDisabledFlag = (statusFlags & 0x04) == 0x04;
+		decimalModeFlag = (statusFlags & 0x08) == 0x08;
+		breakFlag = (statusFlags & 0x10) == 0x10;
+		overflowFlag = (statusFlags & 0x40) == 0x40;
+		negativeFlag = (statusFlags & 0x80) == 0x80;
 	}
 	
 	
-	// Address Modes
+	// --- Address Modes --- //
 	// All functions return the address to data
 	// rather than the actual data.
 	private int immediate()
@@ -954,12 +747,16 @@ public class Cpu
 	
 	private int absoluteX()
 	{
-		return next2Bytes()+xindex;
+		int base = next2Bytes();
+		pageCrossed = ((base & 0xFF) + xindex) > 0xFF;
+		return base+xindex;
 	}
 	
 	private int absoluteY()
 	{
-		return next2Bytes()+yindex;
+		int base = next2Bytes();
+		pageCrossed = ((base & 0xFF) + yindex) > 0xFF;
+		return base+yindex;
 	}
 	
 	private int indirect()
@@ -975,8 +772,9 @@ public class Cpu
 	
 	private int indirectY()
 	{
-		int index = memory[nextByte()];
-		return get2Bytes(index)+yindex;
+		int base = get2Bytes( memory[nextByte()] );
+		pageCrossed = ((base & 0xFF) + yindex) > 0xFF;
+		return base+yindex;
 	}
 	
 	private int next2Bytes()
@@ -1024,14 +822,20 @@ public class Cpu
 	
 	private int signedByteValue(int byteval)
 	{
-		boolean isNegative = (byteval&0x00000080) != 0;
-		byteval &=0x0000007F;
+		boolean isNegative = (byteval&0x80) == 0x80;
+		byteval &=0x7F;
 		return isNegative ? -byteval : byteval;
 	}
 	
 	private void branch(int arg)
 	{
-		pc += signedByteValue(arg);
+		cycles += 1;
+		int offset = signedByteValue(arg);
+		pageCrossed = ((pc & 0xFF) + offset) > 0xFF;
+		if(pageCrossed)
+			cycles += 1;
+		
+		pc += offset;
 	}
 	
 	private void adjustZeroFlag(int result)
@@ -1062,4 +866,8 @@ public class Cpu
 		return memory[stackPointer];
 	}
 	
+	int irqVector()
+	{
+		return get2Bytes(0xFFFE);
+	}
 }

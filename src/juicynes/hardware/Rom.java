48b44a079385e1e3cc3651f7068c8eeb9fe92488
stantonwhite <stantonwhite@744ec148-a6df-11dd-a8c6-677c70d10fe4>
2008-11-20 14:27:58 +0000

    Added capability to load ines roms.

--- src/juicynes/hardware/Rom.java
+++ src/juicynes/hardware/Rom.java
@@ -1,6 +1,12 @@
 package juicynes.hardware;
 
-public class Rom 
-{
+import java.io.IOException;
 
+public abstract class Rom 
+{
+	public abstract void load(String filename) throws IOException;
+	
+	public abstract int getPrgRomCount();
+	
+	public abstract int[] getPrgRom(int i);
 }
